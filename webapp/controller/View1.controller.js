2bb541bc08ab970cf5a3e4f72bf2633961aad5b2
S0017271566 <dgaland@distriplus.be>
2018-03-12 15:34:44 +0000

    1.1

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -12,6 +12,25 @@ sap.ui.define([
 	return Controller.extend("ZADDORDER.controller.View1", {
 
 		onInit: function() {
+			var config = this.getOwnerComponent().getManifest();
+			var sServiceUrl = config["sap.app"].dataSources.ZALL_PLANTS_SRV.uri;
+			var oData = new ODataModel(sServiceUrl, true);
+			var query = "/AddOrderCheckSet";
+			BusyIndicator.show();
+			oData.read(query, null, null, true, function(response) {
+				if (response.results[0].Message !== 'OK') {
+					MessageBox.show(response.results[0].Message, {
+						icon: MessageBox.Icon.INFORMATION,
+						onClose: function() {
+							window.history.go(-1);
+						}
+					});
+				}
+			}, function(error) {
+				MessageBox.error(JSON.parse(error.response.body).error.message.value, {
+					title: "Error"
+				});
+			});
 			this.GetData();
 		},
 
@@ -59,14 +78,14 @@ sap.ui.define([
 			}, function(error) {
 				BusyIndicator.hide();
 				console.log("Error: " + error.response.body.toString());
-				promise.reject();
+				//promise.reject();
 			});
 		},
 		searchArt: function() {
 			var oView = this.getView();
 			var oController = oView.getController();
 			var material = oView.byId("SearchArt").getValue();
-			var URL2 = "/sap/opu/odata/sap/ZCHECK_VALUE_SCAN_SRV/MessageSet(PValue='06" + material + "')";
+			var URL2 = "/sap/opu/odata/sap/ZCHECK_VALUE_SCAN_SRV/MessageSet(PValue='12" + material + "')";
 			debugger;
 			BusyIndicator.show();
 			OData.read(URL2, function(response2) {
@@ -92,12 +111,22 @@ sap.ui.define([
 			}, function(error) {
 				BusyIndicator.hide();
 				console.log("Error: " + error.response.body.toString());
-				promise.reject();
+				//promise.reject();
 			});
 		},
-		GetData: function(material, from) {
+		GetData: function(material, from, number) {
 			var oView = this.getView();
-			var searchString = "A" + material + "/" + "07" + from;
+			debugger;
+			if (number != undefined) {
+				searchString = "M" + "/" + material + "/" + "07" + "/" + number;
+			} else {
+				if (from == undefined) {
+					from = "+";
+					var searchString = "A" + material + "/" + "07" + from;
+				} else {
+					var searchString = "A" + material + "/" + "07" + from;
+				}
+			}
 			material = oView.byId("SearchArt").setValue("");
 			var URL = "/sap/opu/odata/sap/ZPREPARE_FLUX_SRV/ItemsSet?$filter=Zfilter " + "%20eq%20" + "%27" + searchString + "%27&$format=json";
 			debugger;
@@ -133,7 +162,7 @@ sap.ui.define([
 			}, function(error) {
 				BusyIndicator.hide();
 				console.log("Error: " + error.response.body.toString());
-				promise.reject();
+				//promise.reject();
 			});
 		},
 
@@ -190,8 +219,37 @@ sap.ui.define([
 			}, function(error) {
 				BusyIndicator.hide();
 				console.log("Error: " + error.response.body.toString());
-				promise.reject();
+				//promise.reject();
 			});
+		},
+
+		update: function(evt) {
+			var oView = this.getView();
+			var oArticle_input = oView.byId("SearchArt");
+			var id = evt.mParameters.id;
+			debugger;
+			var number = evt.mParameters.selectedItem.getKey();
+			id = id.replace("oSelect", "gtin");
+			var gtin = oView.byId(id).getText();
+			if (!isNaN(number) && number > 0) {
+				this.GetData(gtin, undefined, number);
+				jQuery.sap.delayedCall(500, this, function() {
+					oArticle_input.focus();
+				});
+			} else {
+				var path = $.sap.getModulePath("Press_Shop_Fiori2", "/audio");
+				var aud = new Audio(path + "/MOREINFO.png");
+				aud.play();
+				var infoMsg = oView.getModel("i18n").getResourceBundle().getText("numerical");
+				MessageBox.show(infoMsg, {
+					icon: MessageBox.Icon.ERROR,
+					onClose: function() {
+						jQuery.sap.delayedCall(500, this, function() {
+							oArticle_input.focus();
+						});
+					}
+				});
+			}
 		}
 	});
 });
